7dada95ec6351e8c5b2487a8f547c4efe1a07f72
Yang Haipeng[杨海鹏] <yhp5210@apac.arcsoft.corp>
2016-12-27 17:14:21 +0800

    修改若干bug Author:yanghaipeng

--- GestureUnLock/app/src/main/java/com/example/rainy/gestureunlock/MainActivity.java
+++ GestureUnLock/app/src/main/java/com/example/rainy/gestureunlock/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.rainy.gestureunlock;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.rainy.gestureunlock.view.UnLockView;
+
 public class MainActivity extends Activity {
 
+    private UnLockView mUnLockView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUnLockView = (UnLockView) findViewById(R.id.unlockview);
+        mUnLockView.setmRightPsw("14789");
+
     }
 }
--- GestureUnLock/app/src/main/java/com/example/rainy/gestureunlock/view/UnLockView.java
+++ GestureUnLock/app/src/main/java/com/example/rainy/gestureunlock/view/UnLockView.java
@@ -5,12 +5,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.rainy.gestureunlock.ViewUitl;
+
+import java.util.ArrayList;
+
+import static android.media.CamcorderProfile.get;
 
 /**
  * 手势解锁view
@@ -31,20 +37,34 @@ public class UnLockView extends View {
     private int width;
     private int height;
     public Circle[] circles = new Circle[9];
-    private Paint mGrayPaint;
+    private Paint mPaint;
+    private float mNextX,mNextY;
     private Path mPath = new Path();
+    private String mRightPsw;
+    private StringBuilder mInputPsw;
+    private Context mContext;
+
+
 
     public UnLockView(Context context) {
         super(context);
-        initResources();
+        initResources(context);
     }
     public UnLockView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initResources();
+        initResources(context);
     }
     public UnLockView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initResources();
+        initResources(context);
+    }
+
+    public String getmRightPsw() {
+        return mRightPsw;
+    }
+
+    public void setmRightPsw(String mRightPsw) {
+        this.mRightPsw = mRightPsw;
     }
 
     @Override
@@ -53,25 +73,26 @@ public class UnLockView extends View {
         for(int i=0;i<circles.length;i++){
             Circle circle = circles[i];
             if(circle.isClicked){
-                mGrayPaint.setStyle(Paint.Style.STROKE);
-                mGrayPaint.setColor(Color.parseColor(BLUECOLOR));
-                mGrayPaint.setStrokeWidth(LINE_NORMAL);
+                mPaint.setStyle(Paint.Style.STROKE);
+                mPaint.setColor(Color.parseColor(BLUECOLOR));
+                mPaint.setStrokeWidth(LINE_NORMAL);
                 //画大圆的轮廓
-                canvas.drawCircle(circle.x,circle.y,circle.outterRadius,mGrayPaint);
+                canvas.drawCircle(circle.x,circle.y,circle.outterRadius,mPaint);
             }else{
-                mGrayPaint.setColor(Color.parseColor(GRAYCOLOR));
+                mPaint.setColor(Color.parseColor(GRAYCOLOR));
             }
             //画小圆
-            mGrayPaint.setStyle(Paint.Style.FILL);
-            mGrayPaint.setStrokeWidth(LINE_NORMAL);
-            canvas.drawCircle(circle.x,circle.y,circle.innderRadius,mGrayPaint);
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setStrokeWidth(LINE_NORMAL);
+            canvas.drawCircle(circle.x,circle.y,circle.innderRadius,mPaint);
 
-            //画连线
-            mGrayPaint.setStyle(Paint.Style.STROKE);
-            mGrayPaint.setStrokeWidth(LINE_WIDTH);
-            mGrayPaint.setColor(Color.parseColor(BLUECOLOR));
-            canvas.drawPath(mPath,mGrayPaint);
         }
+        //画连线
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(LINE_WIDTH);
+        mPaint.setColor(Color.parseColor(BLUECOLOR));
+        canvas.drawPath(mPath,mPaint);
+        //画连线
         canvas.save();
     }
 
@@ -95,7 +116,17 @@ public class UnLockView extends View {
                 Log.d(TAG+"onTouchEvent","getX:"+event.getX()+",getRawX="
                         +event.getRawX()+",getY="+event.getY()
                         +",getRawY="+event.getRawY());
-                return true;
+                int index = getClickedIndex(event.getX(),event.getY());
+                if(index >= 0 && index <= circles.length){
+                    gatherInput(index);
+                    mPath.moveTo(circles[index].x,circles[index].y);
+                    return true;
+                }else{
+                    //TODO 第一次没触到任何块则提示
+
+                    return false;
+                }
+
             }
             case MotionEvent.ACTION_MOVE:{
                 Log.d(TAG,"ACTION_MOVE");
@@ -104,20 +135,34 @@ public class UnLockView extends View {
                 int index = getClickedIndex(x,y);
                 if(index >= 0 && index < circles.length){
                     circles[index].isClicked = true;
-                    if(mPath.isEmpty()){
-                        mPath.moveTo(circles[index].x,circles[index].y);
-                    }else{
+                    gatherInput(index);
+                    if(getClickedIndex(mNextX,mNextY) >= 0){
                         mPath.lineTo(circles[index].x,circles[index].y);
+                    }else{
+                        mPath.setLastPoint(circles[index].x,circles[index].y);
                     }
+                    mNextX = circles[index].x;
+                    mNextY = circles[index].y;
                 }else{
-                    mPath.lineTo(x,y);
+                    mNextX = x;
+                    mNextY = y;
+                    mPath.setLastPoint(mNextX,mNextY);
+
                 }
+
                 invalidate();
                 Log.d(TAG+"index","index:"+index);
             }break;
             case MotionEvent.ACTION_UP:{
                 Log.d(TAG,"ACTION_UP");
-                mPath.reset();
+                //TODO 判断密码是否正确
+                if(isInputOK()){
+                    Toast.makeText(mContext, "密码正确:"+mInputPsw, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(mContext, "密码错误:"+mInputPsw, Toast.LENGTH_SHORT).show();
+                }
+                uninit();
+
             }break;
         }
         return super.onTouchEvent(event);
@@ -136,16 +181,6 @@ public class UnLockView extends View {
         for(int i = 0;i<circles.length;i++){
 
             circles[i] = new Circle();
-          /*  if(0 == i%3){
-                circles[i].x = BIGGER_RADIUS;
-            }else{
-                circles[i].x = (i%3)*contentWidth/2;
-            }
-            if(0 == i/3) {
-                circles[i].y = BIGGER_RADIUS;
-            }else{
-                circles[i].y = (i/3)*contentHeight/2;
-            }*/
             circles[i].x = BIGGER_RADIUS + (i%3)*contentWidth/2;
             circles[i].y = BIGGER_RADIUS + (i/3)*contentHeight/2;
             circles[i].isClicked = false;
@@ -153,11 +188,13 @@ public class UnLockView extends View {
             circles[i].outterRadius = BIGGER_RADIUS;
         }
     }
-    public void initResources(){
+    public void initResources(Context context){
 
-        mGrayPaint = new Paint();
-        mGrayPaint.setAntiAlias(true);
-        mGrayPaint.setColor(Color.parseColor(GRAYCOLOR));
+        mContext = context;
+        mInputPsw = new StringBuilder();
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.parseColor(GRAYCOLOR));
     }
 
     public int getClickedIndex(float x,float y){
@@ -172,6 +209,52 @@ public class UnLockView extends View {
             }
         }
         return -1;
+    }
+
+    public void uninit(){
+
+        if(null != circles){
+            for(int i=0;i<circles.length;i++){
+                circles[i].isClicked = false;
+            }
+            invalidate();
+        }
+        if(null != mPath){
+            mPath.reset();
+        }
+        mInputPsw.delete(0,mInputPsw.length());
+
+    }
+
+
+    /**
+     * //得到用户输入的内容
+     * @param index 划过点的下标
+     */
+    public void gatherInput(int index){
+
+        int content = index + 1;
+        if(mInputPsw.length() == 0){
+            mInputPsw.append(content);
+        }else{
+
+            char lastCharacter = mInputPsw.charAt(mInputPsw.length()-1);
+            int iLastValue = Character.getNumericValue(lastCharacter);
+            if(iLastValue != content){
+                mInputPsw.append(content);
+            }
+        }
+
+    }
+
+    /**
+     * 验证输入的密码是否正确
+     * @return 正确返回true
+     */
+    public boolean isInputOK(){
+
+        return mInputPsw == null || mRightPsw == null
+                ? false : mRightPsw.equals(mInputPsw.toString());
     }
 
     static class Circle{
